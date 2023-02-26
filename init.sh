35eee4a0e2e81a96f01dbe79c5627a8b089d5c74
Juca Esmanhoto <jucaesmanhoto@gmail.com>
2023-02-26 19:51:36 -0300

    change init.sh to run docker compose build

--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
 #!bin/bash
 set -vx
 
+docker compose build
 docker start design-auctions-web-1
 docker start design-auctions-db-1
 docker exec -d design-auctions-web-1 sh -c "rails db:create db:migrate db:seed && yarn install && yarn build --watch"
