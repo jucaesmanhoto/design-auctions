39efaa04aff950c7a04f0236f454df69b5b8f8f6
Juca Esmanhoto <jucaesmanhoto@gmail.com>
2023-02-27 04:17:25 -0300

    updates init.sh

--- init.sh
+++ init.sh
@@ -3,5 +3,3 @@ set -vx
 
 docker compose up -d
 docker exec -d design-auctions-web-1 sh -c "rails db:create db:migrate db:seed && yarn install && yarn build --watch"
-docker compose down
-sh start.sh
